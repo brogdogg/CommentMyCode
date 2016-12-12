dc12d75be25bb49c5fcd7b086ad5251c781f1118
Michael Brogdon <brogdogg@gmail.com>
2016-12-11 17:10:08 -0700

    Fleshing out more of the provider interface and adding an additional interface for comment provider

--- Provider/IProvider.cs
+++ Provider/IProvider.cs
@@ -2,24 +2,72 @@
  * File...: IProvider.cs
  * Remarks:
  */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MB.VS.Extension.CommentMyCode.Provider
 {
+
+
   /************************** IProvider **************************************/
   /// <summary>
-  /// 
+  /// Describes the basic provider for the <see cref="CommentMyCode"/> context
   /// </summary>
   public interface IProvider
   {
     /************************ Events *****************************************/
     /************************ Properties *************************************/
+    /// <summary>
+    /// Gets the main context that initialized us
+    /// </summary>
+    CommentMyCode Context { get; }
     /************************ Methods ****************************************/
+    /// <summary>
+    /// Initializes the provider with a given context
+    /// </summary>
+    /// <param name="context"></param>
+    void Initialize(CommentMyCode context);
   } // end of interface - IProvider
+
+
+  /************************** ICommentProvider *******************************/
+  /// <summary>
+  /// 
+  /// </summary>
+  public interface ICommentProvider : IProvider
+  {
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Methods ****************************************/
+    /// <summary>
+    /// Comments the current line, attempting to guess 
+    /// </summary>
+    void Comment();
+
+    /// <summary>
+    /// Comments a class
+    /// </summary>
+    void CommentClass();
+
+    /// <summary>
+    /// Comments an enum
+    /// </summary>
+    void CommentEnum();
+
+    /// <summary>
+    /// Comments a file
+    /// </summary>
+    void CommentFile();
+
+    /// <summary>
+    /// Comments a function
+    /// </summary>
+    void CommentFunction();
+
+    /// <summary>
+    /// Comments a property
+    /// </summary>
+    void CommentProperty();
+  } // end of interface - ICommentProvider
+
 
 }
 
