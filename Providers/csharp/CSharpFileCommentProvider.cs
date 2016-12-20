480233abb2678c87542ac3b279ccd06e350ff984
Michael Brogdon <brogdogg@gmail.com>
2016-12-20 15:04:27 -0700

    Changing the C# file comment provider to use the base abstract file comment provider class

--- Providers/csharp/CSharpFileCommentProvider.cs
+++ Providers/csharp/CSharpFileCommentProvider.cs
@@ -2,6 +2,7 @@
  * File...: CSharpFileCommentProvider.cs
  * Remarks:
  */
+using MB.VS.Extension.CommentMyCode.Extensions;
 using MB.VS.Extension.CommentMyCode.UserOptions;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EnvDTE;
 
 namespace MB.VS.Extension.CommentMyCode.Providers.csharp
 {
@@ -22,7 +24,7 @@ namespace MB.VS.Extension.CommentMyCode.Providers.csharp
   [ExportMetadata("SupportedCommandTypes",
     (int)(SupportedCommandTypeFlag.File))] // Support file comments
   [ExportMetadata("SupportedExtension", ".cs")] // works with *.cs files
-  public class CSharpFileCommentProvider : BaseCommentProvider
+  public class CSharpFileCommentProvider : BaseFileCommentProvider
   {
     /*======================= PUBLIC ========================================*/
     /************************ Events *****************************************/
@@ -37,32 +39,55 @@ namespace MB.VS.Extension.CommentMyCode.Providers.csharp
     /************************ Properties *************************************/
     /************************ Construction ***********************************/
     /************************ Methods ****************************************/
-    protected override void InitializeProvider()
-    {
-    }
 
-    protected override void Process()
+    /*----------------------- ProcessFoot -----------------------------------*/
+    /// <summary>
+    /// Writes out the footer comments for a CSharp file
+    /// </summary>
+    /// <param name="endEditPoint">
+    /// An edit point at the end of the document
+    /// </param>
+    protected override void ProcessFoot(EditPoint endEditPoint)
     {
-      var textDocument = (EnvDTE.TextDocument)Context.Document.Object("TextDocument");
-      var spEditPoint = textDocument.StartPoint.CreateEditPoint();
-      var headerFormat = Context.State.Options.FileHeaderTemplate;
-      InsertLineIntoDoc(spEditPoint, "/" + new String('*', Context.State.Options.MaxColumnWidth - 2));
-      StringNormalizer sn = new StringNormalizer(Context.State.Options.MaxColumnWidth);
-      if (headerFormat != null)
-        foreach(var line in headerFormat.Split('\n'))
-          foreach (var normalizedStr in sn.Normalize(Context.State.MacroExpander.Expand(line), 3))
-            InsertLineIntoDoc(spEditPoint, " * " + normalizedStr);
+      endEditPoint.Insert(
+        "/* End of document - " + Context.Document.Name + " */");
+    } // end of function - ProcessFoot
+
+    /*----------------------- ProcessHead -----------------------------------*/
+    /// <summary>
+    /// Writes out the header comments for a CSharp file
+    /// </summary>
+    /// <param name="startEditPoint">
+    /// The start edit point
+    /// </param>
+    /// <param name="headerTemplate">
+    /// the header template information
+    /// </param>
+    protected override void ProcessHead(EditPoint startEditPoint, string headerTemplate)
+    {
+      var expander = Context.State.MacroExpander;
+      // Insert the beginning of the header comments
+      InsertLineIntoDoc(startEditPoint,
+        "/" + new String('*', Context.State.Options.MaxColumnWidth - 2));
+
+      // Process the header temlate, if there is one
+      if (headerTemplate != null)
+        foreach(var line in headerTemplate.Split('\n'))
+          foreach (var normalizedStr in Normalizer.Normalize(expander.Expand(line), 3))
+            InsertLineIntoDoc(startEditPoint, " * " + normalizedStr);
+      // Or default to our own
       else
       {
-        InsertLineIntoDoc(spEditPoint, " * File...: {FILENAME}");
-        InsertLineIntoDoc(spEditPoint, " * Remarks: ");
-      }
-      InsertLineIntoDoc(spEditPoint, " */");
-      spEditPoint.StartOfDocument();
+        InsertLineIntoDoc(startEditPoint,
+          expander.Expand(" * File...: {FILENAME}"));
+        InsertLineIntoDoc(startEditPoint, " * Remarks: ");
+      } // end of else
 
-      var epEditPoint = textDocument.EndPoint.CreateEditPoint();
-      epEditPoint.Insert("/* End of document - " + Context.Document.Name + " */");
-    }
+      // Close the comment section
+      InsertLineIntoDoc(startEditPoint, " */");
+
+      return;
+    } // end of function - ProcessHead
     /************************ Fields *****************************************/
     /************************ Static *****************************************/
 
