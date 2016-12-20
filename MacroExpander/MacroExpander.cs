224a6d3f6bb600856a1b1755c99bf23eefd57c3e
Michael Brogdon <brogdogg@gmail.com>
2016-12-19 20:53:25 -0700

    Adding a macro expander and a string normalizer, to be utilized for making sure comments stay within boundaries and to have the ability to expand macros specified

---
+++ MacroExpander/MacroExpander.cs
@@ -0,0 +1,107 @@
+﻿/******************************************************************************
+ * File...: MacroExpander.cs
+ * Remarks:
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.VS.Extension.CommentMyCode.MacroExpander
+{
+
+
+  /************************** IMacroExpander *********************************/
+  /// <summary>
+  /// Describes an object reponsible for expanding parsing strings and
+  /// expanding macros referenced within the text
+  /// </summary>
+  public interface IMacroExpander : IDisposable
+  {
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Methods ****************************************/
+    /// <summary>
+    /// Expands the string, expanding macros to their well-known values
+    /// </summary>
+    /// <param name="strToExpand"></param>
+    /// <returns></returns>
+    string Expand(string strToExpand);
+  } // end of interface - IMacroExpander
+
+
+  /************************** MacroExpander **********************************/
+  /// <summary>
+  /// Base implementation of <see cref="IMacroExpander"/>, intended to be used
+  /// as is or inherited and extended
+  /// </summary>
+  public class MacroExpander : IMacroExpander
+  {
+    /*======================= PUBLIC ========================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /*----------------------- MacroExpander ---------------------------------*/
+    /// <summary>
+    /// 
+    /// </summary>
+    public MacroExpander()
+    {
+      return;
+    } // end of function - MacroExpander
+
+    /************************ Methods ****************************************/
+    /*----------------------- Dispose ---------------------------------------*/
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Dispose()
+    {
+      Dispose(true); GC.SuppressFinalize(this);
+    } // end of function - Dispose
+
+    /*----------------------- Expand ----------------------------------------*/
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="strToExpand"></param>
+    /// <returns></returns>
+    public virtual string Expand(string strToExpand)
+    {
+      return strToExpand;
+    } // end of function - Expand
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PROTECTED =====================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /*----------------------- Dispose ---------------------------------------*/
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected virtual void Dispose(bool disposing)
+    {
+      return;
+    } // end of function - Dispose
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PRIVATE =======================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+  } // end of class - MacroExpander
+
+
+} // end of namespace - MB.VS.Extension.CommentMyCode.MacroExpander
+
+
+/* End MacroExpander.cs */
---
+++ StringNormalizer.cs
@@ -0,0 +1,132 @@
+﻿/******************************************************************************
+ * File...: StringNormalizer.cs
+ * Remarks:
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.VS.Extension.CommentMyCode
+{
+
+
+  /************************** StringNormalizer *******************************/
+  /// <summary>
+  /// Normalizes a string, making sure it is only a specified characters long
+  /// creating new strings containing words that go over the boundary
+  /// </summary>
+  public class StringNormalizer
+  {
+    /*======================= PUBLIC ========================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /*----------------------- MaxColumnCount --------------------------------*/
+    /// <summary>
+    /// Gets the maximum column count, where the word boundary is, any words
+    /// beyond this boundary will be inserted into a new line
+    /// </summary>
+    public int MaxColumnCount
+    {
+      get;
+      protected set;
+    } // end of property - MaxColumnCount
+    /************************ Construction ***********************************/
+    /*----------------------- StringNormalizer ------------------------------*/
+    /// <summary>
+    /// Creates a new string normalizer with a maximum column count
+    /// </summary>
+    /// <param name="maxColCnt">
+    /// Optional maximum column count for a word boundary, default is 80
+    /// </param>
+    public StringNormalizer(int maxColCnt = 80)
+    {
+      MaxColumnCount = maxColCnt;
+      return;
+    } // end of function - StringNormalizer
+    /************************ Methods ****************************************/
+    /*----------------------- Normalize -------------------------------------*/
+    /// <summary>
+    /// Normalizes a string, if any words go beyond the specified
+    /// <see cref="MaxColumnCount"/> then a new string will be created with the
+    /// words remaining
+    /// </summary>
+    /// <param name="strToNormalize">
+    /// The string to normalize
+    /// </param>
+    /// <returns>
+    /// A collection of normalized strings to be within the word boundary
+    /// </returns>
+    /// <remarks>
+    /// This method uses an offset of zero
+    /// </remarks>
+    public virtual IEnumerable<string> Normalize(string strToNormalize)
+    {
+      return Normalize(strToNormalize, 0);
+    } // end of function - Normalize
+
+    /*----------------------- Normalize -------------------------------------*/
+    /// <summary>
+    /// Normalizes a string, if any words go beyond the specified
+    /// <see cref="MaxColumnCount"/> then a new string will be created with the
+    /// words remaining
+    /// </summary>
+    /// <param name="strToNormalize">
+    /// The string to normalize
+    /// </param>
+    /// <param name="offset">
+    /// The offset to use when normalizing the string
+    /// </param>
+    /// <returns>
+    /// A collection of normalized strings to be within the word boundary
+    /// </returns>
+    public virtual IEnumerable<string> Normalize(string strToNormalize, int offset)
+    {
+      if (offset < 0 || offset >= MaxColumnCount)
+        throw new InvalidOperationException("Offset is outside the operating range");
+
+      if (strToNormalize != null)
+      {
+        var strCol = strToNormalize.Split(' ');
+        var retval = "";
+        int curLen = offset;
+        foreach(var str in strCol)
+        {
+          if ((curLen = curLen + str.Length) > MaxColumnCount)
+          {
+            curLen = offset;
+            yield return retval;
+            retval = "";
+          }
+          else
+            retval += curLen == 0 ? str : " " + str;
+        } // end of foreach - string in the split collection
+        if (curLen > 0)
+          yield return retval;
+      } // end of if - valid string to normalize
+    } // end of function - Normalize
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PROTECTED =====================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PRIVATE =======================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+  } // end of class - StringNormalizer
+
+}
+
+
+/* End StringNormalizer.cs */
