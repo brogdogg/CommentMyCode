ec500607913d1fac0fe65266c2e024cc49469e24
Michael Brogdon <brogdogg@gmail.com>
2016-12-25 22:07:16 -0700

    Adding a function for the C# function provider to find the line to start inserting comments

--- Providers/csharp/CSharpFunctionCommentProvider.cs
+++ Providers/csharp/CSharpFunctionCommentProvider.cs
@@ -44,6 +44,21 @@ namespace MB.VS.Extension.CommentMyCode.Providers.csharp
       Debug.WriteLine("Function: " + Context.CodeElement.FullName);
       Debug.WriteLine("Start Line: " + Context.CodeElement.StartPoint.Line);
       Debug.WriteLine("End line:   " + Context.CodeElement.EndPoint.Line);
+      var line = LineToInsert();
+      Debug.WriteLine("Line to start Insert: " + line);
+    }
+
+    protected virtual int LineToInsert()
+    {
+      var td = Context.Document.Object("TextDocument") as EnvDTE.TextDocument;
+      var ts = Context.Document.GetTextSelection();
+      var fcm = Context.Document.ProjectItem.FileCodeModel;
+      var ep = td.CreateEditPoint(ts.ActivePoint);
+      ep.MoveToLineAndOffset(ep.Line - 1, ep.LineCharOffset);
+      
+      while(fcm.CodeElementFromPoint(ep, vsCMElement.vsCMElementAttribute) == null)
+        ep.MoveToLineAndOffset(ep.Line - 1, ep.LineCharOffset);
+      return ts.CurrentLine;
     }
 
     /************************ Fields *****************************************/
