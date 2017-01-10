ce21f1a5ab698f56aa00a5b1517812c4eaa81506
Michael Brogdon <brogdogg@gmail.com>
2017-01-09 22:28:54 -0700

    Initial support for c/c++ source files

---
+++ Providers/c/CCommentProvider.cs
@@ -0,0 +1,73 @@
+﻿/******************************************************************************
+ * File...: CCommentProvider.cs
+ * Remarks:
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.VS.Extension.CommentMyCode.Providers.c
+{
+
+
+  /************************** CCommentProvider *******************************/
+  /// <summary>
+  /// 
+  /// </summary>
+  [Export(typeof(ICommentProvider))] // Indicate we implement ICommentProvider
+  [ExportMetadata("SupportedCommandTypes",
+    (int)(SupportedCommandTypeFlag.Function))] // Support file comments
+  [ExportMetadata("SupportedExtensions", new string[] { ".cpp", ".c", ".h", ".hpp" })] // works with *.cs files
+  public class CCommentProvider : BaseCommentProvider
+  {
+    /*======================= PUBLIC ========================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /*----------------------- CCommentProvider ------------------------------*/
+    /// <summary>
+    /// Default constructor setting up open/close comment strings for the base
+    /// </summary>
+    public CCommentProvider() : base("/*", "*/")
+    {
+    } // end of function - CCommentProvider
+
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PROTECTED =====================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /*----------------------- Process ---------------------------------------*/
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void Process()
+    {
+      Debug.WriteLine("CCommentProvider.Process-->");
+      Debug.WriteLine("CCommentProvider.Process<--");
+    } // end of function - Process
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+
+    /*======================= PRIVATE =======================================*/
+    /************************ Events *****************************************/
+    /************************ Properties *************************************/
+    /************************ Construction ***********************************/
+    /************************ Methods ****************************************/
+    /************************ Fields *****************************************/
+    /************************ Static *****************************************/
+  } // end of class - CCommentProvider
+
+
+}
+
+
+/* End CCommentProvider.cs */
